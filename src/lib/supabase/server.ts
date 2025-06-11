66f31287ae6fd7c4509f1899f054723c5a56fbb0
Roman CHANDET <116293425+RomanCtrl@users.noreply.github.com>
2025-06-11 10:45:02 +0200

    update authentification flow

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,18 +1,18 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
+import type { Database } from './database.types.ts'
 
-export function createClient() {
+export const getSupabaseServerClient = () => {
   const cookieStore = cookies()
-
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
           return cookieStore.get(name)?.value
-        },
-      },
+        }
+      }
     }
   )
 }
